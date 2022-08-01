f9b247d88d4e183bbe007c3f64e6a9dd7b92c314
schedulerz <thisisabilkair.zhakupov@gmail.com>
2022-08-01 21:57:19 +0600

    Number #1

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char a = 99;
+        char b = 104;
+        char c = 97;
+        char d = 114;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
     }
 }
